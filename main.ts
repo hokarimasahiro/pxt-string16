e5c067b1ebc50fb29a190e5cd6c852ae569cb3aa
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-07-06 04:14:16 +0900

    create

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 color=#1eb0f0 icon="\u3041" block="text"
+namespace text {
+
+    //% shim=text::charcodeat16
+    function charcodeat16(s:string,n: number): number {
+        return 0;
+    }
+    /**
+      * charcode at by utf16 code
+      * @param s original string, eg: "abcdefg"
+      * @param n data position, eg: 3
+      */
+    //% blockId=char_code_at16 block="char code at %n in %s by utf16"
+    export function charCodeAt16(s:string,n: number): number {
+        return (charcodeat16(s,n));
+    }
+}
