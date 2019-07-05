5041d6fb1fb9b632769a85c4444ea3e194f95b93
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-07-06 05:46:20 +0900

    update

--- char16.cpp
+++ char16.cpp
@@ -1,8 +1,8 @@
 #include "pxt.h"
 
 namespace char16 {
-  uint32_t getNumber16At(unsigned char s[],int32_t n)
+  uint32_t getNumber16At(String s,int32_t n)
   {
-	return s[n*2]*256 + s[n*2+1];
+	return 0;
   }
 }
